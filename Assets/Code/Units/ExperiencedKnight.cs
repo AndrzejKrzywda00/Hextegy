e4bb499a8ae6021b6aad4b0f764ac005364457c5
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2022-05-20 12:39:03 +0200

    added instantiation code to exp night and removed size of noElement

--- Assets/Code/Units/ExperiencedKnight.cs
+++ Assets/Code/Units/ExperiencedKnight.cs
@@ -1,5 +1,11 @@
 using UnityEngine;
 
-public class ExperiencedKnight : MonoBehaviour {
-    
+public class ExperiencedKnight : MonoBehaviour
+{
+    public static void Put(HexCell cell)
+    {
+        ExperiencedKnight experiencedKnight = Resources.Load<ExperiencedKnight>("ExperiencedKnight");
+        cell.prefab = experiencedKnight;
+    }
+
 }
