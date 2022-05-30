6a4116e789afdb5eafd9095f0df351a52c5ac005
Szymik <szymonszymiczek32@gmail.com>
2022-05-30 20:26:33 +0200

    debug

--- Assets/Code/Control/UIButtonControl.cs
+++ Assets/Code/Control/UIButtonControl.cs
@@ -3,19 +3,39 @@ using UnityEngine;
 public class UIButtonControl : MonoBehaviour {
     
     private PlayerController _playerController;
-    public string prefabName;
 
     private void Start() {
         _playerController = FindObjectOfType<PlayerController>();
     }
 
-    public void OnUnitButtonClick() {
-        _playerController.prefabFromUI = _playerController.prefabFromUI == null ? Resources.Load<CellObject>(prefabName) : null;
+    public void OnUnit1ButtonClick() {
+        _playerController.prefabFromUI = _playerController.prefabFromUI == null ? Resources.Load<CellObject>("CommonKnight") : null;
         _playerController.selectedCellWithUnit = null;
     }
     
+    public void OnUnit2ButtonClick() {
+        _playerController.prefabFromUI = _playerController.prefabFromUI == null ? Resources.Load<CellObject>("ExperiencedKnight") : null;
+        _playerController.selectedCellWithUnit = null;
+    }
+    
+    public void OnUnit3ButtonClick() {
+        _playerController.prefabFromUI = _playerController.prefabFromUI == null ? Resources.Load<CellObject>("LegendaryKnight") : null;
+        _playerController.selectedCellWithUnit = null;
+    }
+    
+    
     public void OnBuildingButtonClick() {
-        _playerController.prefabFromUI = _playerController.prefabFromUI == null ? Resources.Load<CellObject>(prefabName) : null;
+        _playerController.prefabFromUI = _playerController.prefabFromUI == null ? Resources.Load<CellObject>("House") : null;
+        _playerController.selectedCellWithUnit = null;
+    }
+    
+    public void OnTower1ButtonClick() {
+        _playerController.prefabFromUI = _playerController.prefabFromUI == null ? Resources.Load<CellObject>("NormalTower") : null;
+        _playerController.selectedCellWithUnit = null;
+    }
+    
+    public void OnTower2ButtonClick() {
+        _playerController.prefabFromUI = _playerController.prefabFromUI == null ? Resources.Load<CellObject>("SuperTower") : null;
         _playerController.selectedCellWithUnit = null;
     }
 }
