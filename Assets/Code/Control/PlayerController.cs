6b57d2985157d46d9beac242d799c33f9243eba3
MForystek <micforystek@student.agh.edu.pl>
2022-05-30 19:36:52 +0200

    mini refactor

--- Assets/Code/Control/PlayerController.cs
+++ Assets/Code/Control/PlayerController.cs
@@ -129,7 +129,7 @@ public class PlayerController : MonoBehaviour {
     
     private bool IsCellInUnitMovementRange(HexCell hexCell) {
         HexCoordinates[] path = _pf.OptionalPathFromTo(selectedCellWithUnit, hexCell);
-        CellObject unit = (CellObject) selectedCellWithUnit.prefabInstance;
+        CellObject unit = selectedCellWithUnit.prefabInstance;
         if (path == null) return false;
         return path.Length - 1 <= unit.Range();
     }
