09fceb30dcae0d9f8af8b0ef6c1439f41ea90dc2
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2022-06-01 20:38:30 +0200

    added player manager

---
+++ Assets/Code/Control/Game/PlayerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Code.Control.Game {
+    
+    public class PlayerManager {
+
+        private static HexCell[] _gridCells;
+        private static List<HexCell> _playerCells = new List<HexCell>();
+
+        public PlayerManager(HexCell[] gridCells) {
+            _gridCells = gridCells;
+            _playerCells = new List<HexCell>();
+        }
+        
+        public void LoadPlayer(int playerId) {
+            IEnumerable<HexCell> playerIdQuery = from cell in _gridCells where cell.playerId.Equals(playerId) select cell;
+            _playerCells.AddRange(playerIdQuery);
+        }
+        
+        public int GetNumberOfUnits() {
+            IEnumerable<HexCell> unitsQuery = from cell in _playerCells where cell.HasUnit() select cell;
+            return unitsQuery.Count();
+        }
+
+        public int GetNumberOfEmptyCells() {
+            IEnumerable<HexCell> emptyCellsQuery = from cell in _playerCells where cell.IsEmpty() select cell;
+            return emptyCellsQuery.Count();
+        }
+
+        public int GetNumberOfFarms() {
+            IEnumerable<HexCell> farmsQuery = from cell in _playerCells where cell.HasTower() select cell;
+            return farmsQuery.Count();
+        }
+    }
+    
+}
