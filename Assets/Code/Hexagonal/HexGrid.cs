a0aa7138dd585546c79cd72cbb468c7ee06eef9f
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2022-06-06 12:08:56 +0200

    set HexGrid class to use namespace

--- Assets/Code/Hexagonal/HexGrid.cs
+++ Assets/Code/Hexagonal/HexGrid.cs
@@ -3,177 +3,178 @@ using Code.CellObjects;
 using Code.Control.Game;
 using Code.Generator;
 using Code.Generator.Objects;
-using Code.Hexagonal;
 using UnityEngine;
 
-public class HexGrid : MonoBehaviour {
+namespace Code.Hexagonal {
+    public class HexGrid : MonoBehaviour {
     
-    public static int NumberOfPlayers = 4;
-    private const int GridWidth = 30;
-    private const int GridHeight = 30;
+        public static int NumberOfPlayers = 4;
+        private const int GridWidth = 30;
+        private const int GridHeight = 30;
 
-    public HexCell hexCellPrefab;
+        public HexCell hexCellPrefab;
 
-    private PlayerController _playerController;
-    private Camera _cam;
-    private HexMesh _hexMesh;
-    private HexCell[] _cells;
-    private Cell[] _cellPrototypes;
+        private PlayerController _playerController;
+        private Camera _cam;
+        private HexMesh _hexMesh;
+        private HexCell[] _cells;
+        private Cell[] _cellPrototypes;
 
-    public HexCell[] Cells => _cells;
+        public HexCell[] Cells => _cells;
 
-    public HexCell[] GetNeighborsOfCell(HexCell hexCell) {
-        List<HexCell> neighbors = new List<HexCell>();
-        HexCoordinates[] neighborsCoordinates = hexCell.NeighborsCoordinates();
+        public HexCell[] GetNeighborsOfCell(HexCell hexCell) {
+            List<HexCell> neighbors = new List<HexCell>();
+            HexCoordinates[] neighborsCoordinates = hexCell.NeighborsCoordinates();
         
-        foreach (HexCoordinates coordinates in neighborsCoordinates) {
-            neighbors.Add(CellAtCoordinates(coordinates));
+            foreach (HexCoordinates coordinates in neighborsCoordinates) {
+                neighbors.Add(CellAtCoordinates(coordinates));
+            }
+
+            return neighbors.ToArray();
         }
 
-        return neighbors.ToArray();
-    }
-
-    private void Awake() {
-        _playerController = FindObjectOfType<PlayerController>();
-        _cam = Camera.main;
-        _hexMesh = GetComponentInChildren<HexMesh>();
-        _cellPrototypes = GenerateMap();
-        CreateCells();
-    }
-
-    private static Cell[] GenerateMap() {
-        GridGenerator generator = new GridGenerator();
-
-        generator.GeneratePlayerFields(NumberOfPlayers, 4);
-        // you can change parameters here!!!
-        //generator.scale = 1f;
-        //generator.fulfil = 0.3f;
-        //generator.playerFields.Add(new GridGenerator.PlayerField(1,2));
-        //generator.treeRatio = 0.7f;
-        
-        return generator.GenerateMap(GridHeight, GridWidth);
-    }
-
-    private void Start() {
-        _hexMesh.Triangulate(_cells);
-    }
-
-    private void Update() {
-        if (Input.GetMouseButtonDown(0)) {
-            HandleInput();
+        private void Awake() {
+            _playerController = FindObjectOfType<PlayerController>();
+            _cam = Camera.main;
+            _hexMesh = GetComponentInChildren<HexMesh>();
+            _cellPrototypes = GenerateMap();
+            CreateCells();
         }
-    }
 
-    private void CreateCells() {
-        _cells = new HexCell[GridHeight * GridWidth];
-        FillCells();
-    }
+        private static Cell[] GenerateMap() {
+            GridGenerator generator = new GridGenerator();
 
-    private void FillCells() {
-        for (int z = 0, i = 0; z < GridHeight; z++) {
-            for (int x = 0; x < GridWidth; x++) {
-                CreateCell(x, z, i++);
+            generator.GeneratePlayerFields(NumberOfPlayers, 4);
+            // you can change parameters here!!!
+            //generator.scale = 1f;
+            //generator.fulfil = 0.3f;
+            //generator.playerFields.Add(new GridGenerator.PlayerField(1,2));
+            //generator.treeRatio = 0.7f;
+        
+            return generator.GenerateMap(GridHeight, GridWidth);
+        }
+
+        private void Start() {
+            _hexMesh.Triangulate(_cells);
+        }
+
+        private void Update() {
+            if (Input.GetMouseButtonDown(0)) {
+                HandleInput();
             }
         }
-    }
 
-    private void CreateCell(int x, int z, int i) {
-        Vector3 position = CreateCellPosition(x, z);
-        InstantiateCellOnGrid(x, z, i, position);
-    }
-
-    private Vector3 CreateCellPosition(int x, int z) {
-        return new Vector3 {
-            x = (x + z * 0.5f - Mathf.Floor((float)z / 2)) * (HexMetrics.InnerRadius * 2f),
-            y = 0f,
-            z = z * (HexMetrics.OuterRadius * 1.5f)
-        };
-    }
-
-    private void InstantiateCellOnGrid(int x, int z, int i, Vector3 position) {
-        int prototypeIndex = GetCellIndexByPosition(x, z);
-        if (_cellPrototypes[prototypeIndex] == null) return;
-        
-        HexCell hexCell = _cells[i] = Instantiate(hexCellPrefab);
-        PositionTheCellOnGrid(x, z, position, hexCell);
-        MapPrototypeToHexCell(prototypeIndex, hexCell);
-    }
-
-    private void PositionTheCellOnGrid(int x, int z, Vector3 position, HexCell hexCell) {
-        Transform cellTransform = hexCell.transform;
-        cellTransform.SetParent(transform, false);
-        cellTransform.localPosition = position;
-        hexCell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
-    }
-
-    public Dictionary<int, int> MapCellsToInitialBalanceOfPlayers() {
-
-        Dictionary<int, int> playersInitialBalances = new Dictionary<int, int>();
-        
-        foreach (Cell cellPrototype in _cellPrototypes) {
-            if (cellPrototype == null) continue;
-            if (!playersInitialBalances.ContainsKey(cellPrototype.PlayerId)) playersInitialBalances.Add(cellPrototype.PlayerId, 0);
-            if (!cellPrototype.HasTree()) playersInitialBalances[cellPrototype.PlayerId] += 1;
+        private void CreateCells() {
+            _cells = new HexCell[GridHeight * GridWidth];
+            FillCells();
         }
 
-        return playersInitialBalances;
-    }
-
-    private int GetCellIndexByPosition(int x, int z) {
-        return x * GridWidth + z;
-    }
-
-    private int GetCellIndexByHexCoordinates(HexCoordinates coordinates) {
-        return coordinates.Z * GridWidth + coordinates.X + coordinates.Z / 2;
-    }
-
-    public HexCell CellAtCoordinates(HexCoordinates coordinates) {
-        int index = GetCellIndexByHexCoordinates(coordinates);
-        if (index < _cells.Length && index >= 0) return _cells[index];
-        return null;
-    }
-
-    private void MapPrototypeToHexCell(int prototypeIndex, HexCell hexCell) {
-        hexCell.PutOnCell(_cellPrototypes[prototypeIndex].Prefab);
-        hexCell.playerId = _cellPrototypes[prototypeIndex].PlayerId;
-    }
-
-    private void HandleInput() {
-        Ray inputRay = _cam.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(inputRay, out RaycastHit hit)) InteractWithCell(hit.point);
-    }
-
-    private void InteractWithCell(Vector3 position) {
-        int cellIndex = GetCellIndexFromPosition(position);
-        if (_cells[cellIndex] != null) _playerController.Handle(_cells[cellIndex]);
-        _hexMesh.Triangulate(_cells);
-    }
-
-    private int GetCellIndexFromPosition(Vector3 position) {
-        position = transform.InverseTransformPoint(position);
-        HexCoordinates hexCoordinates = HexCoordinates.FromPosition(position);
-        int cellIndex = GetCellIndexByHexCoordinates(hexCoordinates);
-        return cellIndex;
-    }
-
-    public void GenerateTreesNextToExistingTrees() {
-        foreach (HexCell cell in _cells) if(cell != null && cell.HasBigTree()) GenerateTreeInRandomAdjacentCell(cell);
-    }
-
-    private void GenerateTreeInRandomAdjacentCell(HexCell hexCell) {
-        int randomIndex = Random.Range(0, HexMetrics.NumOfTrianglesInHexagon);
-        int index = 0;
-        HexCoordinates[] neighborsCoordinates = hexCell.NeighborsCoordinates();
-        foreach (HexCoordinates coordinates in neighborsCoordinates) {
-            HexCell neighbor = CellAtCoordinates(coordinates);
-            if (neighbor != null && index++ == randomIndex && neighbor.IsEmpty()) {
-                HandleAddingTreeToCell(neighbor);
-            } 
+        private void FillCells() {
+            for (int z = 0, i = 0; z < GridHeight; z++) {
+                for (int x = 0; x < GridWidth; x++) {
+                    CreateCell(x, z, i++);
+                }
+            }
         }
-    }
 
-    private void HandleAddingTreeToCell(HexCell neighbor) {
-        neighbor.PutOnCell(Prefabs.GetTree());
-        MoneyManager.DecrementBalanceOfPlayer(neighbor.playerId);
+        private void CreateCell(int x, int z, int i) {
+            Vector3 position = CreateCellPosition(x, z);
+            InstantiateCellOnGrid(x, z, i, position);
+        }
+
+        private Vector3 CreateCellPosition(int x, int z) {
+            return new Vector3 {
+                x = (x + z * 0.5f - Mathf.Floor((float)z / 2)) * (HexMetrics.InnerRadius * 2f),
+                y = 0f,
+                z = z * (HexMetrics.OuterRadius * 1.5f)
+            };
+        }
+
+        private void InstantiateCellOnGrid(int x, int z, int i, Vector3 position) {
+            int prototypeIndex = GetCellIndexByPosition(x, z);
+            if (_cellPrototypes[prototypeIndex] == null) return;
+        
+            HexCell hexCell = _cells[i] = Instantiate(hexCellPrefab);
+            PositionTheCellOnGrid(x, z, position, hexCell);
+            MapPrototypeToHexCell(prototypeIndex, hexCell);
+        }
+
+        private void PositionTheCellOnGrid(int x, int z, Vector3 position, HexCell hexCell) {
+            Transform cellTransform = hexCell.transform;
+            cellTransform.SetParent(transform, false);
+            cellTransform.localPosition = position;
+            hexCell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        }
+
+        public Dictionary<int, int> MapCellsToInitialBalanceOfPlayers() {
+
+            Dictionary<int, int> playersInitialBalances = new Dictionary<int, int>();
+        
+            foreach (Cell cellPrototype in _cellPrototypes) {
+                if (cellPrototype == null) continue;
+                if (!playersInitialBalances.ContainsKey(cellPrototype.PlayerId)) playersInitialBalances.Add(cellPrototype.PlayerId, 0);
+                if (!cellPrototype.HasTree()) playersInitialBalances[cellPrototype.PlayerId] += 1;
+            }
+
+            return playersInitialBalances;
+        }
+
+        private int GetCellIndexByPosition(int x, int z) {
+            return x * GridWidth + z;
+        }
+
+        private int GetCellIndexByHexCoordinates(HexCoordinates coordinates) {
+            return coordinates.Z * GridWidth + coordinates.X + coordinates.Z / 2;
+        }
+
+        public HexCell CellAtCoordinates(HexCoordinates coordinates) {
+            int index = GetCellIndexByHexCoordinates(coordinates);
+            if (index < _cells.Length && index >= 0) return _cells[index];
+            return null;
+        }
+
+        private void MapPrototypeToHexCell(int prototypeIndex, HexCell hexCell) {
+            hexCell.PutOnCell(_cellPrototypes[prototypeIndex].Prefab);
+            hexCell.playerId = _cellPrototypes[prototypeIndex].PlayerId;
+        }
+
+        private void HandleInput() {
+            Ray inputRay = _cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(inputRay, out RaycastHit hit)) InteractWithCell(hit.point);
+        }
+
+        private void InteractWithCell(Vector3 position) {
+            int cellIndex = GetCellIndexFromPosition(position);
+            if (_cells[cellIndex] != null) _playerController.Handle(_cells[cellIndex]);
+            _hexMesh.Triangulate(_cells);
+        }
+
+        private int GetCellIndexFromPosition(Vector3 position) {
+            position = transform.InverseTransformPoint(position);
+            HexCoordinates hexCoordinates = HexCoordinates.FromPosition(position);
+            int cellIndex = GetCellIndexByHexCoordinates(hexCoordinates);
+            return cellIndex;
+        }
+
+        public void GenerateTreesNextToExistingTrees() {
+            foreach (HexCell cell in _cells) if (cell != null && cell.HasBigTree()) GenerateTreeInRandomAdjacentCell(cell);
+        }
+
+        private void GenerateTreeInRandomAdjacentCell(HexCell hexCell) {
+            int randomIndex = Random.Range(0, HexMetrics.NumOfTrianglesInHexagon);
+            int index = 0;
+            HexCoordinates[] neighborsCoordinates = hexCell.NeighborsCoordinates();
+            foreach (HexCoordinates coordinates in neighborsCoordinates) {
+                HexCell neighbor = CellAtCoordinates(coordinates);
+                if (neighbor != null && index++ == randomIndex && neighbor.IsEmpty()) {
+                    HandleAddingTreeToCell(neighbor);
+                } 
+            }
+        }
+
+        private void HandleAddingTreeToCell(HexCell neighbor) {
+            neighbor.PutOnCell(Prefabs.GetTree());
+            MoneyManager.DecrementBalanceOfPlayer(neighbor.playerId);
+        }
     }
 }
