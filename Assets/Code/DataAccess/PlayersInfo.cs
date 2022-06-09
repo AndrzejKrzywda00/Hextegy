2fb2a79fd409a754c015f83c0999c5fac155c4a4
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2022-06-09 15:32:29 +0200

    Adjusted PlayersInfo.cs to store info about capitals of players

--- Assets/Code/DataAccess/PlayersInfo.cs
+++ Assets/Code/DataAccess/PlayersInfo.cs
@@ -1,7 +1,20 @@
+using System.Collections.Generic;
+using Code.Hexagonal;
+
 namespace Code.DataAccess {
     public static class PlayersInfo {
+        
         public static int[] DeadPlayersId = {};
         public static int Winner = 0;
+        private static Dictionary<int, HexCell> PlayersCapitals = new Dictionary<int, HexCell>();
 
+        public static void SetPlayerCapital(int playerId, HexCell capital) {
+            PlayersCapitals.Add(playerId, capital);
+        }
+
+        public static HexCell GetPlayerCapital(int playerId) {
+            return PlayersCapitals.ContainsKey(playerId) ? PlayersCapitals[playerId] : null;
+        }
+        
     }
 }
