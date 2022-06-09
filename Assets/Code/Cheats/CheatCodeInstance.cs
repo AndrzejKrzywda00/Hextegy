2351031aacf048ccd43ca18f68b6a1908412e8e4
MForystek <micforystek@student.agh.edu.pl>
2022-06-09 19:47:39 +0200

    Music between the scenes fixed and turning on and off sounds added

---
+++ Assets/Code/Cheats/CheatCodeInstance.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.Events;
+
+namespace Code.Cheats {
+    [System.Serializable]
+    public class CheatCodeInstance {
+        public string code;
+        public UnityEvent cheatEvent;
+    }
+}
