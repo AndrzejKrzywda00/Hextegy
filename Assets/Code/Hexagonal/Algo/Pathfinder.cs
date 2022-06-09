836202b514dc8812b247a144c82acbb9e315676a
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2022-06-09 18:34:46 +0200

    Units dying works better but still on always perfect

--- Assets/Code/Hexagonal/Algo/Pathfinder.cs
+++ Assets/Code/Hexagonal/Algo/Pathfinder.cs
@@ -18,6 +18,7 @@ namespace Code.Hexagonal.Algo {
         private List<Node> _closedList;
         private HexGrid _grid;
         private bool _searchEnded;
+        private bool _searchOnlyThroughFriendlyCells;
 
         private void Awake() {
             _grid = FindObjectOfType<HexGrid>();
@@ -30,12 +31,14 @@ namespace Code.Hexagonal.Algo {
         }
 
         public HexCoordinates[] ConsistentPathFromTo(HexCell from, HexCell to) {
-            HexCoordinates[] path = PathFromTo(from, to);
-            if (path.Contains(from.coordinates) && path.Contains(to.coordinates) && PathConsistsOfFriendlyCellsWithPlayer(from.playerId, path)) return path;
+           _searchOnlyThroughFriendlyCells = true; 
+           HexCoordinates[] path = PathFromTo(from, to);
+           if (path.Contains(from.coordinates) && path.Contains(to.coordinates) && PathConsistsOfFriendlyCellsWithPlayer(from.playerId, path)) return path;
             return null;
         }
 
         private HexCoordinates[] PathFromTo(HexCell from, HexCell to) {
+            _searchOnlyThroughFriendlyCells = false;
             InitializePathfindingProcessParameters(from, to);
             return CalculatePathFromTo();
         }
@@ -66,7 +69,7 @@ namespace Code.Hexagonal.Algo {
         }
 
         private void HandleNeighborCells(HexCoordinates[] coordinates, Node parentNode) {
-            List<HexCell> neighborCells = GenerateNeighbors(coordinates);
+            List<HexCell> neighborCells = GenerateNeighbors(coordinates, parentNode);
 
             foreach (HexCell neighbor in neighborCells) {
                 if (IsDestination(neighbor)) {
@@ -81,20 +84,24 @@ namespace Code.Hexagonal.Algo {
             }
         }
 
-        private List<HexCell> GenerateNeighbors(HexCoordinates[] coordinates) {
+        private List<HexCell> GenerateNeighbors(HexCoordinates[] coordinates, Node parentNode) {
             List<HexCell> neighborCells = new List<HexCell>();
         
             foreach (HexCoordinates coordinate in coordinates) {
                 HexCell hexCell = _grid.CellAtCoordinates(coordinate);
-                if(CellNotFeasible(hexCell)) continue;
+                if (CellNotFeasible(hexCell, parentNode)) continue;
                 neighborCells.Add(hexCell);
             }
 
             return neighborCells;
         }
 
-        private static bool CellNotFeasible(HexCell hexCell) {
-            return CellDoesntExist(hexCell);
+        private bool CellNotFeasible(HexCell hexCell, Node parentNode) {
+            if (CellDoesntExist(hexCell)) return true;
+            if (_searchOnlyThroughFriendlyCells) {
+                return !hexCell.IsFriendlyCellWith(parentNode.GetCell.playerId);
+            }
+            return false;
         }
 
         private static bool CellDoesntExist(HexCell hexCell) {
