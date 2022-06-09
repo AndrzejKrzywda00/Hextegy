314d3938bddda8f386b63e908d6420d91d045b80
Szymik <szymonszymiczek32@gmail.com>
2022-06-09 19:54:53 +0200

    fix

--- Assets/Code/DataAccess/Settings.cs
+++ Assets/Code/DataAccess/Settings.cs
@@ -9,7 +9,7 @@ namespace Code.DataAccess {
         public static float Fulfill = 0.5f;
         public static float TreeRatio = 0.2f;
 
-        public static List<int> AlivePlayersId = new List<int>();
+        public static List<int> AlivePlayersId = new List<int>() {1, 2};
         public static int Winner = 2;
     }
 }
