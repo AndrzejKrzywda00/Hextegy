91c9cc8cef1c7e4ef3fa29efd3e5ad21f96f097a
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2022-06-09 10:52:58 +0200

    Quick fix of lack of ability to choose any object from menu besides Unit tier 1

--- Assets/Code/Control/UI/UIButtonControl.cs
+++ Assets/Code/Control/UI/UIButtonControl.cs
@@ -17,33 +17,33 @@ namespace Code.Control.UI {
         }
     
         public void OnUnitTier2ButtonClick() {
-            _playerController.prefabFromUI = _playerController.prefabFromUI == Prefabs.GetUnitTier2() ? Prefabs.GetUnitTier2() : null;
+            _playerController.prefabFromUI = _playerController.prefabFromUI != Prefabs.GetUnitTier2() ? Prefabs.GetUnitTier2() : null;
             _playerController.selectedCellWithUnit = null;
         }
     
         public void OnUnitTier3ButtonClick() {
-            _playerController.prefabFromUI = _playerController.prefabFromUI == Prefabs.GetUnitTier3() ? Prefabs.GetUnitTier3() : null;
+            _playerController.prefabFromUI = _playerController.prefabFromUI != Prefabs.GetUnitTier3() ? Prefabs.GetUnitTier3() : null;
             _playerController.selectedCellWithUnit = null;
         }
         
         public void OnUnitTier4ButtonClick() {
-            _playerController.prefabFromUI = _playerController.prefabFromUI == Prefabs.GetUnitTier4() ? Prefabs.GetUnitTier4() : null;
+            _playerController.prefabFromUI = _playerController.prefabFromUI != Prefabs.GetUnitTier4() ? Prefabs.GetUnitTier4() : null;
             _playerController.selectedCellWithUnit = null;
         }
     
     
         public void OnFarmButtonClick() {
-            _playerController.prefabFromUI = _playerController.prefabFromUI == Prefabs.GetFarm() ? Prefabs.GetFarm() : null;
+            _playerController.prefabFromUI = _playerController.prefabFromUI != Prefabs.GetFarm() ? Prefabs.GetFarm() : null;
             _playerController.selectedCellWithUnit = null;
         }
     
         public void OnTowerTier1ButtonClick() {
-            _playerController.prefabFromUI = _playerController.prefabFromUI == Prefabs.GetTowerTier1() ? Prefabs.GetTowerTier1() : null;
+            _playerController.prefabFromUI = _playerController.prefabFromUI != Prefabs.GetTowerTier1() ? Prefabs.GetTowerTier1() : null;
             _playerController.selectedCellWithUnit = null;
         }
     
         public void OnTowerTier2ButtonClick() {
-            _playerController.prefabFromUI = _playerController.prefabFromUI == Prefabs.GetTowerTier2() ? Prefabs.GetTowerTier2() : null;
+            _playerController.prefabFromUI = _playerController.prefabFromUI != Prefabs.GetTowerTier2() ? Prefabs.GetTowerTier2() : null;
             _playerController.selectedCellWithUnit = null;
         }
     }
